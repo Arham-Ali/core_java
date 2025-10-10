5f64137570ef858197625fdfb596f21d979b8b76
Arham-Ali <arhamali1best@gmail.com>
2025-10-10 19:38:38 +0530

    first commit

---
+++ src/com/stringexamples/Example1.java
@@ -0,0 +1,23 @@
+package com.stringexamples;
+
+public class Example1 {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "Hello";
+        System.out.println(a==b);   //True
+
+        String s = new String(original "Method");
+        String r = new String(original "Method");
+
+        // ==, used to compare by reference
+        System.out.println(a==b);   //true
+        System.out.println(s==r);   //true
+
+        // equals(): to compare by values
+        System.out.println(a.equals(b));   //true
+        System.out.println(s.equals(r));   //true
+
+
+    }
+
+}
