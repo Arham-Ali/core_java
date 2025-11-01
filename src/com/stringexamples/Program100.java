837ae00e31ec930ddb12e84a7622c97b8cc88245
Arham-Ali <arhamali1best@gmail.com>
2025-11-01 19:14:56 +0530

    09-11-2025

---
+++ src/com/stringexamples/Program100.java
@@ -0,0 +1,17 @@
+package com.stringexamples;//package com.stringexamples;
+//
+//import java.util.Locale;
+//
+//public class Program6 {
+//    public static void main (String[] args)
+//    {
+//        String s= "World Wide Web";
+//        s = s.toUpperCase();    // a = "WORLD WIDE WEB"
+//        int sum = 0;
+//
+//        String regex;
+//        String [] r = s.split(regex);
+//
+//
+//    }
+//}
