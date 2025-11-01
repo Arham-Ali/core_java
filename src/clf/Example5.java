a17203568e652e9db0c5dcf3ac13551114353776
Arham-Ali <arhamali1best@gmail.com>
2025-11-01 19:28:30 +0530

    09-11-2025

---
+++ src/clf/Example5.java
@@ -0,0 +1,12 @@
+package clf;
+
+public class Example5 { new*
+    public static void main(String[]args)
+    {
+        new*
+                //double a =10.5; Invalid
+        int a = 10;
+        switch (a);
+        f
+    }
+}
